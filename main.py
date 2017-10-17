40ff47047ca443c1dcffd9048f87b08a4a8c883f
lpelczar <lukepelczar@gmail.com>
2017-10-17 14:56:01 +0200

    Create main skeleton

--- main.py
+++ main.py
@@ -1,30 +1,54 @@
 import sys
+import os
 from geometry import *
+
+
+def handle_first_menu_option(shapes):
+    pass
+
+
+def handle_second_menu_option(shapes):
+    pass
+
+
+def handle_third_menu_option(shapes):
+    pass
+
+
+def handle_fourth_menu_option(shapes):
+    pass
+
+
+def handle_fifth_menu_option(shapes):
+    pass
+
+
+def handle_sixth_menu_option(shapes):
+    sys.exit()
+
+
+OPTIONS = {
+    '1': ['Add new shape', handle_first_menu_option],
+    '2': ['Show all shapes', handle_second_menu_option],
+    '3': ['Show shape with the largest perimeter', handle_third_menu_option],
+    '4': ['Show shape with the largest area', handle_fourth_menu_option],
+    '5': ['Show formulas', handle_fifth_menu_option],
+    '0': ['Exit program', handle_sixth_menu_option]}
 
 
 def main():
 
-    shapes = ShapeList()  # object containing all shapes added by the user
+    os.system('clear')
+    shapes = ShapeList()
     while True:
-        # TODO: implement user interaction here. You can change the code below
-        option = input("Select an option: ")
-        if option == "1":
-            # Add new shape
-            pass
-        elif option == "2":
-            # Show all shapes
-            pass
-        elif option == "3":
-            # Show shape with the largest perimeter
-            pass
-        elif option == "4":
-            # Show shape with the largest area
-            pass
-        elif option == "5":
-            # Show formulas
-            pass
-        elif option == "0":
-            sys.exit()
+        for key, value in OPTIONS.items():
+            print('(' + key + ') ' + value[0])
+        user_input = input("Type number of option: ")
+        if (user_input not in OPTIONS):
+            os.system('clear')
+        else:
+            os.system('clear')
+            OPTIONS[user_input][1](shapes)
 
 
 if __name__ == "__main__":
